14f30c051a29964efa885324c03051965642e7a6
ahmed <ahmedabdulmawjood@cmail.carleton.ca>
2024-09-17 15:16:25 -0400

    second commit. added print statement and created removing BuddyInfo in AddressBook main mthod

--- src/BuddyInfo.java
+++ src/BuddyInfo.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class AddressBook {
     private List<BuddyInfo> buddies;
 
@@ -25,45 +24,21 @@ public class AddressBook {
             System.out.println(buddy);
         }
     }
-}
-
-
-public class BuddyInfo {
-    private String name;
-    private String address;
-    private String phoneNumber;
-
-
-
-    public BuddyInfo() {
-        this("Default Name", "Default Address", "000-000-0000");
-    }
-
-    public BuddyInfo(String name, String address, String phoneNumber) {
-        this.name = name;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
 
     public static void main(String[] args) {
-        BuddyInfo buddy = new BuddyInfo();
-        buddy.name = "John";
-        buddy.address = "123 Street";
-        buddy.phoneNumber = "123-456-7890";
-        System.out.println("Hello " + buddy.getName());
-        System.out.println("Address: " + buddy.getAddress());
-        System.out.println("Phone Number: " + buddy.getPhoneNumber());
+        AddressBook addressBook = new AddressBook();
+
+        // First buddy
+        BuddyInfo buddy1 = new BuddyInfo("John", "123 Street", "123-456-7890");
+        addressBook.addBuddy(buddy1);
+        addressBook.printBuddies();
+        System.out.println("Address book is ready!");
+
+        // Second buddy - add and then remove
+        BuddyInfo buddy2 = new BuddyInfo("Jane", "456 Avenue", "987-654-3210");
+        addressBook.addBuddy(buddy2);
+        System.out.println("Added Jane to the address book.");
+        addressBook.removeBuddy(buddy2);
+        System.out.println("Removed Jane from the address book.");
     }
 }
