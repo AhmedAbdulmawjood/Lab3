8588e9eb83003c1bedf67f794d69e99f7cff42ca
ahmed <ahmedabdulmawjood@cmail.carleton.ca>
2024-09-17 16:38:58 -0400

    Seperated the two files. Originally had them together.

---
+++ src/AddressBook.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+    private List<BuddyInfo> buddies;
+
+    public AddressBook() {
+        this.buddies = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo buddy) {
+        if (buddy != null) {
+            this.buddies.add(buddy);
+        }
+    }
+
+    public void removeBuddy(BuddyInfo buddy) {
+        this.buddies.remove(buddy);
+    }
+
+    // Optionally, add a method to print all buddies
+    public void printBuddies() {
+        for (BuddyInfo buddy : buddies) {
+            System.out.println(buddy);
+        }
+    }
+
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+
+        // First buddy
+        BuddyInfo buddy1 = new BuddyInfo("John", "123 Street", "123-456-7890");
+        addressBook.addBuddy(buddy1);
+        addressBook.printBuddies();
+        System.out.println("Address book is ready!");
+
+        // Second buddy - add and then remove
+        BuddyInfo buddy2 = new BuddyInfo("Jane", "456 Avenue", "987-654-3210");
+        addressBook.addBuddy(buddy2);
+        System.out.println("Added Jane to the address book.");
+        addressBook.removeBuddy(buddy2);
+        System.out.println("Removed Jane from the address book.");
+    }
+}
--- src/BuddyInfo.java
+++ src/BuddyInfo.java
@@ -1,45 +1,39 @@
-import java.util.ArrayList;
-import java.util.List;
+public class BuddyInfo {
+    private String name;
+    private String address;
+    private String phoneNumber;
 
-public class AddressBook {
-    private List<BuddyInfo> buddies;
-
-    public AddressBook() {
-        this.buddies = new ArrayList<>();
+    public BuddyInfo() {
+        this("Default Name", "Default Address", "000-000-0000");
     }
 
-    public void addBuddy(BuddyInfo buddy) {
-        if (buddy != null) {
-            this.buddies.add(buddy);
-        }
+    public BuddyInfo(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
     }
 
-    public void removeBuddy(BuddyInfo buddy) {
-        this.buddies.remove(buddy);
+    public String getName() {
+        return name;
     }
 
-    // Optionally, add a method to print all buddies
-    public void printBuddies() {
-        for (BuddyInfo buddy : buddies) {
-            System.out.println(buddy);
-        }
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "BuddyInfo{name='" + name + "', address='" + address + "', phoneNumber='" + phoneNumber + "'}";
     }
 
     public static void main(String[] args) {
-        AddressBook addressBook = new AddressBook();
-
-        // First buddy
-        BuddyInfo buddy1 = new BuddyInfo("John", "123 Street", "123-456-7890");
-        addressBook.addBuddy(buddy1);
-        addressBook.printBuddies();
-        System.out.println("Address book is ready!");
-
-        // Second buddy - add and then remove
-        BuddyInfo buddy2 = new BuddyInfo("Jane", "456 Avenue", "987-654-3210");
-        addressBook.addBuddy(buddy2);
-        System.out.println("Added Jane to the address book.");
-        addressBook.removeBuddy(buddy2);
-        System.out.println("Removed Jane from the address book.");
-
+        BuddyInfo buddy = new BuddyInfo("John", "123 Street", "123-456-7890");
+        System.out.println("Hello " + buddy.getName());
+        System.out.println("Address: " + buddy.getAddress());
+        System.out.println("Phone Number: " + buddy.getPhoneNumber());
     }
 }
